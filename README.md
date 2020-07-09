4b9a0aa50094cc3c84cbce89fde85197a32b85ab
Hunter Freyer <yt@hjfreyer.com>
2020-07-09 14:58:40 -0400

    Tweaks

---
+++ README.md
@@ -0,0 +1,7 @@
+# hjfreyer's bashrc
+
+To install:
+
+```
+curl https://raw.githubusercontent.com/hjfreyer/homerc/master/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-cd /tmp/
+set -x
+
+tmp_dir=$(mktemp -d -t homerc-XXXXXXXXXX)
+
+cd $tmp_dir
 wget https://github.com/hjfreyer/homerc/archive/master.zip
 unzip master.zip
 mkdir -p $HOME/.bashrc.d
 mv homerc-master/bashrc.d/* $HOME/.bashrc.d/
 
-grep -qxF 'source "$HOME/.bashrc.d/*"' $HOME/.bashrc || echo 'source "$HOME/.bashrc.d/*"' >> $HOME/.bashrc
+grep -qxF 'source $HOME/.bashrc.d/*' $HOME/.bashrc || echo 'source $HOME/.bashrc.d/*' >> $HOME/.bashrc
