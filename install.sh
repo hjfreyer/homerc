c2c6a261f99e7f9cef8baa01e0e04341f162a9c3
Hunter Freyer <yt@hjfreyer.com>
2020-09-12 19:49:52 -0400

    shell quoting is the best

--- install.sh
+++ install.sh
@@ -13,6 +13,6 @@ unzip master.zip
 mv homerc-master "$HOME/.homerc"
 ln -s "$HOME/.homerc/bashrc.d/" "$HOME/.bashrc.d"
 
-SOURCE_CMD='for f in "$HOME/.bashrc.d/*.sh"; do source "$f"; done'
+SOURCE_CMD='for f in $HOME/.bashrc.d/*.sh; do source "$f"; done'
 
 grep -qxF "$SOURCE_CMD" $HOME/.bashrc || echo "$SOURCE_CMD" >> $HOME/.bashrc
