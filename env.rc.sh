143053bc4bf6ebff151ba739a4ec108735b31495
Hunter Freyer <hjfreyer@google.com>
2022-01-26 17:03:07 +0000

    Tidying

--- env.rc.sh
+++ env.rc.sh
@@ -1,9 +1,8 @@
-
-export EDITOR="emacs -nw --no-init-file"
 export PATH="$HOME/.local/bin:$PATH"
-export HISTCONTROL=ignoredups
-
-export GOPATH="$HOME/go"
 export PATH="$HOME/go/bin:$PATH"
 
+export GOPATH="$HOME/go"
+
+export EDITOR="emacs -nw --no-init-file"
+export HISTCONTROL=ignoredups
 export PS1='\h:\w$(__git_ps1)\$ '
