b34baf8dee80ef4aca3fbca3042635d6aa1075e8
Hunter Freyer <hjfreyer@google.com>
2022-11-01 14:05:13 +0000

    Combine alias and env into a single file.

--- bashrc.sh
+++ bashrc.sh
@@ -1,3 +1,24 @@
-source "$(dirname "${BASH_SOURCE[0]}")/alias.rc.sh"
-source "$(dirname "${BASH_SOURCE[0]}")/env.rc.sh"
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/go/bin:$PATH"
 export PATH="$(dirname "${BASH_SOURCE[0]}")/bin":$PATH
+
+export GOPATH="$HOME/go"
+
+export EDITOR="emacs -nw"
+export HISTCONTROL=ignoredups
+export PS1='\h:\w$(__git_ps1)\$ '
+
+alias ll="ls -lah"
+alias cd="pushd"
+alias pd="popd"
+
+alias rebash="exec $SHELL"
+
+alias gits="git status"
+alias gitwipe="git reset --hard HEAD"
+alias gcam="git commit -a -m 'commit'"
+alias stgs="stg series"
+
+alias e=$EDITOR
+
+
--- env.rc.sh
+++
@@ -1,8 +0,0 @@
-export PATH="$HOME/.local/bin:$PATH"
-export PATH="$HOME/go/bin:$PATH"
-
-export GOPATH="$HOME/go"
-
-export EDITOR="emacs -nw --no-init-file"
-export HISTCONTROL=ignoredups
-export PS1='\h:\w$(__git_ps1)\$ '
