6635efb2be4fae92af658564ead43e2a2cd9ac03
Hunter Freyer <hjfreyer@google.com>
2023-02-22 18:28:16 +0000

    Move omzsh custom stuff under version control

--- init.zsh
+++ init.zsh
@@ -64,7 +64,7 @@ DISABLE_UNTRACKED_FILES_DIRTY="true"
 # HIST_STAMPS="mm/dd/yyyy"
 
 # Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
+ZSH_CUSTOM="$SCRIPT_DIR/oh-my-zsh-custom"
 
 # Which plugins would you like to load?
 # Standard plugins can be found in $ZSH/plugins/
